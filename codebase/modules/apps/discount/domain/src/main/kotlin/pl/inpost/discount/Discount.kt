b651641f2cec07438b45c69424153a33e799c2a3
Bartosz Surma <bartosz.surma@tesco.com>
2024-07-09 10:26:56 +0200

    chore: make PercentageDiscount a data class

--- codebase/modules/apps/discount/domain/src/main/kotlin/pl/inpost/discount/Discount.kt
+++ codebase/modules/apps/discount/domain/src/main/kotlin/pl/inpost/discount/Discount.kt
@@ -11,7 +11,7 @@ data object ZeroDiscount : Discount {
     override fun apply(price: Money): Money = Money(amount)
 }
 
-class PercentageDiscount (override val amount: Double): Discount {
+data class PercentageDiscount (override val amount: Double): Discount {
     constructor(amount: Int) : this(amount.toDouble())
 
     override fun apply(price: Money): Money =
@@ -20,23 +20,4 @@ class PercentageDiscount (override val amount: Double): Discount {
             Money.ZERO == price -> Money.ZERO
             else -> price.percentage(amount)
         }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as PercentageDiscount
-
-        return amount == other.amount
-    }
-
-    override fun hashCode(): Int {
-        return amount.hashCode()
-    }
-
-    override fun toString(): String {
-        return "PercentageDiscount(amount=$amount)"
-    }
-
-
 }
